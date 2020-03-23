66ce9e91d2d32e6620fb51723f36d4737ed87911
octalife2020 <pedro@octa.life>
2020-03-23 09:57:50 -0300

    iniciando repositório

---
+++ curl.php
@@ -0,0 +1,15 @@
+<?php
+
+//phpinfo();
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com.br");
+
+if(curl_exec($ch) === false){
+    echo curl_error($ch);
+}
+else{
+    echo "Sucesso";
+}
+curl_close($ch);
