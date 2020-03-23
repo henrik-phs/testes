6d457ad47dc64a35cf89dde5eb36894ad309cc22
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2020-03-23 11:28:53 -0300

    Update boleto.php

--- boleto.php
+++ boleto.php
@@ -5,18 +5,18 @@ require __DIR__ . "/vendor/autoload.php";
 $pagarme = new \PagarMe\Client("ak_test_bWX2vmWL8FnObv4rgkE6tl5IzFEzEZ");
 
 $transaction = $pagarme->transactions()->create([
-  'amount' => 1000,
+  'amount' => 20000,
   'payment_method' => 'boleto',
-  'async' => false,
+  'async' => true,
   'customer' => [
     'external_id' => '1',
-    'name' => 'Nome do cliente',
+    'name' => 'Pedro',
     'type' => 'individual',
     'country' => 'br',
     'documents' => [
       [
         'type' => 'cpf',
-        'number' => '00000000000'
+        'number' => '00000000001'
       ]
     ],
     'phone_numbers' => [ '+551199999999' ],
