70e86c7f96810e36bb98d58aca9971d1d3280ac4
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2020-03-23 10:28:39 -0300

    Update curl.php

--- curl.php
+++ curl.php
@@ -4,7 +4,7 @@
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://www.google.com.br");
+curl_setopt($ch, CURLOPT_URL, "http://www.google.com.br");
 
 if(curl_exec($ch) === false){
     echo curl_error($ch);
