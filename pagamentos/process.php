537c71b095e50da6ee6718c1d338d55f2f237495
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2020-03-24 08:39:46 -0300

    Update process.php

--- pagamentos/process.php
+++ pagamentos/process.php
@@ -5,87 +5,66 @@ $pagarme = new PagarMe\Client('ak_test_bWX2vmWL8FnObv4rgkE6tl5IzFEzEZ');
 
 $dados = filter_var_array($_POST, FILTER_SANITIZE_STRIPPED);
 
-var_dump($pagarme);
-$cartao = true;
-if($cartao) {
-    $transaction = $pagarme->transactions()->create([
-        'amount' => $dados["amount"],
-        'card_id' => 'card_ci6l9fx8f0042rt16rtb477gj',
-        'payment_method' => 'credit_card',
-        'postback_url' => 'http://requestb.in/pkt7pgpk',
-        'customer' => [
-          'external_id' => '0001',
-          'name' => $dados["titular"], 
-          'email' => $dados["email"],
-          'type' => 'individual',
-            'country' => 'br',
-            'documents' => [
-              [
-                'type' => 'cpf',
-                'number' => $dados["cpf"]
-              ]
-            ],
-            'phone_numbers' => [ '+551199999999' ]
-        ],
-        'billing' => [
-            'name' => $dados["nome"],
-            'address' => [
-              'country' => 'br',
-              'street' => $dados["rua"],
-              'street_number' => $dados["numero"],
-              'state' => $dados["estado"],
-              'city' => $dados["cidade"],
-              'neighborhood' => $dados["bairro"],
-              'zipcode' => $dados["cep"]
-            ]
-        ],
-        'shipping' => [
-            'name' => $dados["nome"],
-            'fee' => 1020,
-            'delivery_date' => '2020-04-22',
-            'expedited' => false,
-            'address' => [
-              'country' => 'br',
-              'street' => $dados["rua"],
-              'street_number' => $dados["numero"],
-              'state' => $dados["estado"],
-              'city' => $dados["cidade"],
-              'neighborhood' => $dados["bairro"],
-              'zipcode' => $dados["cep"]
-            ]
-        ],
-        'items' => [
-            [
-              'id' => $dados["itemid"],
-              'title' => $dados["title"],
-              'unit_price' => $dados["price"],
-              'quantity' => $dados["quantity"],
-              'tangible' => true
-            ]
+$transaction = $pagarme->transactions()->create([
+  'amount' => 100,
+  'card_id' => 'card_ci6l9fx8f0042rt16rtb477gj',
+  'payment_method' => 'credit_card',
+  'postback_url' => 'http://requestb.in/pkt7pgpk',
+  'customer' => [
+    'external_id' => '0001',
+    'name' => 'Aardvark Silva', 
+    'email' => 'aardvark.silva@pagar.me',
+    'type' => 'individual',
+      'country' => 'br',
+      'documents' => [
+        [
+          'type' => 'cpf',
+          'number' => '67415765095'
         ]
-      ]);
-
-      var_dump($transaction);
-}
-
-if($dados["btn-boleto"]) {
-    $transaction = $pagarme->transactions()->create([
-        'amount' => 1000,
-        'payment_method' => 'boleto',
-        'async' => false,
-        'customer' => [
-          'external_id' => '1',
-          'name' => 'Nome do cliente',
-          'type' => 'individual',
-          'country' => 'br',
-          'documents' => [
-            [
-              'type' => 'cpf',
-              'number' => '00000000000'
-            ]
-          ],
-          'phone_numbers' => [ '+551199999999' ],
-          'email' => 'cliente@email.com'
-        ]
-      ]);
-}
+      ],
+      'phone_numbers' => [ '+551199999999' ]
+  ],
+  'billing' => [
+      'name' => 'Nome do pagador',
+      'address' => [
+        'country' => 'br',
+        'street' => 'Avenida Brigadeiro Faria Lima',
+        'street_number' => '1811',
+        'state' => 'sp',
+        'city' => 'Sao Paulo',
+        'neighborhood' => 'Jardim Paulistano',
+        'zipcode' => '01451001'
+      ]
+  ],
+  'shipping' => [
+      'name' => 'Nome de quem receberá o produto',
+      'fee' => 1020,
+      'delivery_date' => '2018-09-22',
+      'expedited' => false,
+      'address' => [
+        'country' => 'br',
+        'street' => 'Avenida Brigadeiro Faria Lima',
+        'street_number' => '1811',
+        'state' => 'sp',
+        'city' => 'Sao Paulo',
+        'neighborhood' => 'Jardim Paulistano',
+        'zipcode' => '01451001'
+      ]
+  ],
+  'items' => [
+      [
+        'id' => '1',
+        'title' => 'R2D2',
+        'unit_price' => 300,
+        'quantity' => 1,
+        'tangible' => true
+      ],
+      [
+        'id' => '2',
+        'title' => 'C-3PO',
+        'unit_price' => 700,
+        'quantity' => 1,
+        'tangible' => true
+      ]
+  ]
+]);
