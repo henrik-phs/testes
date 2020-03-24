189ba421e9da333ba4ffe0aa7de4ef9b988f799f
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2020-03-24 08:49:28 -0300

    Update process.php

--- pagamentos/process.php
+++ pagamentos/process.php
@@ -5,68 +5,11 @@ $pagarme = new PagarMe\Client('ak_test_bWX2vmWL8FnObv4rgkE6tl5IzFEzEZ');
 
 $dados = filter_var_array($_POST, FILTER_SANITIZE_STRIPPED);
 
-$transaction = $pagarme->transactions()->create([
-  'amount' => 100,
-  'card_id' => 'card_ci6l9fx8f0042rt16rtb477gj',
-  'payment_method' => 'credit_card',
-  'postback_url' => 'http://requestb.in/pkt7pgpk',
-  'customer' => [
-    'external_id' => '0001',
-    'name' => 'Aardvark Silva', 
-    'email' => 'aardvark.silva@pagar.me',
-    'type' => 'individual',
-      'country' => 'br',
-      'documents' => [
-        [
-          'type' => 'cpf',
-          'number' => '67415765095'
-        ]
-      ],
-      'phone_numbers' => [ '+551199999999' ]
-  ],
-  'billing' => [
-      'name' => 'Nome do pagador',
-      'address' => [
-        'country' => 'br',
-        'street' => 'Avenida Brigadeiro Faria Lima',
-        'street_number' => '1811',
-        'state' => 'sp',
-        'city' => 'Sao Paulo',
-        'neighborhood' => 'Jardim Paulistano',
-        'zipcode' => '01451001'
-      ]
-  ],
-  'shipping' => [
-      'name' => 'Nome de quem receberá o produto',
-      'fee' => 1020,
-      'delivery_date' => '2018-09-22',
-      'expedited' => false,
-      'address' => [
-        'country' => 'br',
-        'street' => 'Avenida Brigadeiro Faria Lima',
-        'street_number' => '1811',
-        'state' => 'sp',
-        'city' => 'Sao Paulo',
-        'neighborhood' => 'Jardim Paulistano',
-        'zipcode' => '01451001'
-      ]
-  ],
-  'items' => [
-      [
-        'id' => '1',
-        'title' => 'R2D2',
-        'unit_price' => 300,
-        'quantity' => 1,
-        'tangible' => true
-      ],
-      [
-        'id' => '2',
-        'title' => 'C-3PO',
-        'unit_price' => 700,
-        'quantity' => 1,
-        'tangible' => true
-      ]
-  ]
+$getCreditCard = $pagarme->cards()->create([
+  'holder_name' => 'Yoda',
+  'number' => '5167244180341689',
+  'expiration_date' => '1121',
+  'cvv' => '159'
 ]);
 
-var_dump($transaction);
+var_dump($getCreditCard);
